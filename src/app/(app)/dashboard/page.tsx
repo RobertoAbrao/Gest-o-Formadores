c018a19c68d074442bc3d0643f12603956433ced
Roberto Abrão Pereira <beto-a-p@hotmail.com>
2025-10-31 13:45:06 +0000

    Agora eu preciso que mostre o que deve ser feito, por exemplo, se está c

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -320,7 +320,7 @@ export default function DashboardPage() {
                                  </div>
                                   <Button variant="link" size="sm" asChild className='h-auto p-0 text-blue-600'>
                                       <Link href={`/relatorio/${formacao.id}`}>
-                                        Ver Relatório
+                                         {formacao.status === 'pos-formacao' ? 'Concluir' : 'Arquivar'}
                                       </Link>
                                   </Button>
                                </li>
